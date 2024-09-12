e5e0ab91169ab7021531a9d6f424a2645ccc3434
Manarivo RANDRIAMAROMANANA <94703788+ManarivoRa@users.noreply.github.com>
2024-09-12 16:18:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -55,11 +55,20 @@ def moncamambert():
 
 
 
+@app.route('/commits/')
+def commits():
+    return render_template('commits.html')
+
 @app.route('/extract-minutes/<date_string>')
 def extract_minutes(date_string):
-        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-        minutes = date_object.minute
-        return jsonify({'minutes': minutes})
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
+
+def fetch_commits():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    response = requests.get(url)
+    return response.json()
 
   
 if __name__ == "__main__":
